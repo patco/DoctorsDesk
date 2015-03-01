c178ed09ccf44626f0b0521d3756a9d3b3114265
patco <kpatakas@gmail.com>
2015-03-01 16:50:58 +0200

    Some little stuff to patient

--- src/main/java/com/patco/doctorsdesk/server/domain/entities/Patient.java
+++ src/main/java/com/patco/doctorsdesk/server/domain/entities/Patient.java
@@ -2,6 +2,7 @@ package com.patco.doctorsdesk.server.domain.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -9,6 +10,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -22,31 +24,38 @@ import com.patco.doctorsdesk.server.domain.entities.base.DBEntity;
 public class Patient extends DBEntity implements Serializable {
 
 	private static final long serialVersionUID = 6797130500240757054L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String comments;
-	
+
 	@NotNull
-	@Temporal( TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date created;
-	
+
 	@NotNull
 	@NotEmpty
 	private String name;
-	
+
 	@NotNull
 	@NotEmpty
 	private String surname;
-	
+
 	@NotNull
-	@OneToOne(cascade=CascadeType.ALL, mappedBy="patient", fetch=FetchType.LAZY)
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "patient", fetch = FetchType.LAZY)
 	private Medicalhistory medicalhistory;
-	
-	public Integer getId() 			{	return this.id;	}
-	
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "patient", fetch = FetchType.LAZY)
+	private Set<Address> addresses;
+
+	public Patient() {}
+
+	public Integer getId() {
+		return this.id;
+	}
+
 	@Override
 	public String getXML() {
 		// TODO Auto-generated method stub
